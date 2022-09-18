906896a09b724dac91d7582cd4aecf0ca258ced5
Kevin Qu <kevinqu88@gmail.com>
2022-09-17 23:13:36 -0400

    css

--- code/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ code/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,28 +1,45 @@
 import React, { useState } from 'react';
-
+import styles from './CourseInput.module.css';
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
 
+  const [isInputValid, setIsInputValid] = useState(true);
+
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
+    setIsInputValid(event.target.value.trim().length !== 0);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
-    props.onAddGoal(enteredValue);
+    if (enteredValue.trim().length === 0) {
+      setIsInputValid(false);
+    } else {
+      setEnteredValue('');
+      props.onAddGoal(enteredValue);
+    }
   };
 
+  const validGreen = (validStatus) => {
+    return validStatus ? 'valid' : 'invalid';
+  }
+
   return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+    <div>
+      <form onSubmit={formSubmitHandler}>
+      <div className={styles['form-control'] + ' ' + styles[validGreen(isInputValid)]}>
+        <label >Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler}
+          value={enteredValue} />
       </div>
-      <Button type="submit">Add Goal</Button>
+      
+      { isInputValid ? 'valid' : 'invalid'}
     </form>
+      <Button type="button" primary={true} onClickz={formSubmitHandler} isValid={isInputValid}>Add Goal</Button>
+    </div>
+    
   );
 };
 
--- code/01-starting-project/src/components/UI/Button/Button.js
+++ code/01-starting-project/src/components/UI/Button/Button.js
@@ -1,13 +1,46 @@
 import React from 'react';
-
+import styled, {css} from 'styled-components';
 import './Button.css';
 
+const SubmitButton = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: ${props => props.isValid ? 'pointer' : 'not-allowed'};
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+  
+  ${props => {
+    if (props.isValid) {
+      return css`
+        color: green;
+      `
+    } else {
+      return `color: red`
+    }
+  }}
+`;
+
 const Button = props => {
+
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <SubmitButton onClick={props.onClickz} primary={props.primary} isValid={props.isValid}>
       {props.children}
-    </button>
+    </SubmitButton>
   );
+
 };
 
 export default Button;

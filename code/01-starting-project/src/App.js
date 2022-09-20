adb74962bbaa6a86d888b17bb38d57e2417f167b
Kevin Qu <kevinqu88@gmail.com>
2022-09-19 22:18:57 -0400

    f

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -1,19 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+
+  
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+  useEffect(() => {
+    const userLoginInfo = localStorage.getItem("isLoggedIn");
+    setIsLoggedIn(userLoginInfo === '1');
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.setItem("isLoggedIn", "0");
     setIsLoggedIn(false);
   };
 
